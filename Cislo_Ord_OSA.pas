570955d5853df9edb6b694f7da897f443132036f
Pavel Chludil <74764576+pchludil@users.noreply.github.com>
2023-02-16 22:07:32 +0100

    v1.1

---
+++ Cislo_Ord_OSA.pas
@@ -0,0 +1,26 @@
+unit Cislo_Ord_OSA;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
